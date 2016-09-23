1d8d99833cec347110fc2c6362072f9699b689ec
Julio Garcia <jgarcia@bluebeam.com>
2016-09-22 21:04:16 -0700

    Modifying deps file to include Hyperlink

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -1133,6 +1133,67 @@ L.Draw.Marker = L.Draw.Feature.extend({
 });
 
 
+L.Draw.Hyperlink = L.Draw.SimpleShape.extend({
+	statics: {
+		TYPE: 'hyperlink'
+	},
+
+	options: {
+		shapeOptions: {
+			stroke: true,
+			color: '#f06eaa',
+			weight: 4,
+			opacity: 0.5,
+			fill: true,
+			fillColor: null, //same as color by default
+			fillOpacity: 0.2,
+			clickable: true
+		},
+		metric: true // Whether to use the metric measurement system or imperial
+	},
+
+	initialize: function (map, options) {
+		// Save the type so super can fire, need to do this as cannot do this.TYPE :(
+		this.type = L.Draw.Hyperlink.TYPE;
+
+		this._initialLabelText = L.drawLocal.draw.handlers.hyperlink.tooltip.start;
+
+		L.Draw.SimpleShape.prototype.initialize.call(this, map, options);
+	},
+
+	_drawShape: function (latlng) {
+		if (!this._shape) {
+			this._shape = new L.Rectangle(new L.LatLngBounds(this._startLatLng, latlng), this.options.shapeOptions);
+			this._map.addLayer(this._shape);
+		} else {
+			this._shape.setBounds(new L.LatLngBounds(this._startLatLng, latlng));
+		}
+	},
+
+	_fireCreatedEvent: function () {
+		var rectangle = new L.Rectangle(this._shape.getBounds(), this.options.shapeOptions);
+		L.Draw.SimpleShape.prototype._fireCreatedEvent.call(this, rectangle);
+	},
+
+	_getTooltipText: function () {
+		var tooltipText = L.Draw.SimpleShape.prototype._getTooltipText.call(this),
+			shape = this._shape,
+			latLngs, area, subtext;
+
+		if (shape) {
+			latLngs = this._shape.getLatLngs();
+			area = L.GeometryUtil.geodesicArea(latLngs);
+			subtext = L.GeometryUtil.readableArea(area, this.options.metric);
+		}
+
+		return {
+			text: tooltipText.text,
+			subtext: subtext
+		};
+	}
+});
+
+
 L.Edit = L.Edit || {};
 
 L.Edit.Marker = L.Handler.extend({
