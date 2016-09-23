98481503b3e3dafe890e5e72fd0346fd3583329c
Julio Garcia <jgarcia@bluebeam.com>
2016-09-22 21:40:09 -0700

    InnerHTML fix

--- dist/leaflet.draw-src.js
+++ dist/leaflet.draw-src.js
@@ -2993,7 +2993,7 @@ L.Toolbar = L.Class.extend({
 		}
 
 		if (options.glyphicon) {
-			link.innerHTML = '<i class="glyphicon"'  + options.glyphicon + '"/>';
+			link.innerHTML = L.DomUtil.create('i', 'glyphicon ' + options.glyphicon);
 		}
 
 		if (options.title) {
